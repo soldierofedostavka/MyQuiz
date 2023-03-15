fc80b299b2199fd095620d3fcc4490e4967c3483
Lesha Zhoglik <lehazhoglik@gmail.com>
2023-03-15 13:57:07 +0300

    asdsa

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const task = {
-  question: 'Ты придурок?',
-  answers: [ 'Да', 'Нет', 'А ты?', 'Пошла в дупу' ],
-  correctAnswer: 0
-}
+const tasks = 
+[{
+  question: `<h2>Кто на картинке?</h2><img src="leha.jpg" alt="leha">`,
+  answers: [ 'Кошка', 'Лёшка', 'Антошка', '1С программист' ],
+  correctAnswer: 1
+},
+{
+  question: `<h2>Что будет, если дать Лёшке кружку пива?</h2>`,
+  answers: [ 'Он издаст звук ПииииуууУ', 'Он упадёт на колени', 'Он убежит в слезах в галактику?', 'Всё сразу' ],
+  correctAnswer: 3
+}]
 
-const quest = document.querySelector('h2')
-quest.innerHTML = task.question
+const quest = document.querySelector('.ask')
+quest.innerHTML = tasks[0].question
 
-const answers = document.querySelectorAll('.answer')
-console.log(task.answers[1].a1)
-answers.forEach((e, i) => {
-  e.innerHTML = task.answers[i]
+const $answers = document.querySelectorAll('.answer')
+$answers.forEach((e, i) => {
+  e.innerHTML = tasks[0].answers[i]
+  e.addEventListener('click', () => {
+    if (i == tasks[0].correctAnswer) {
+      quest.innerHTML = tasks[1].question
+      for (let i = 0; i < $answers.length; i++) {
+        $answers[i].innerHTML = tasks[1].answers[i]
+      }
+    }
+  })
 })
