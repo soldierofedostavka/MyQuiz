bfacde8be88d4f0e1ad905bba2c5ae8188049119
Lesha Zhoglik <lehazhoglik@gmail.com>
2023-03-15 10:31:53 +0300

    dva

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('test')
+const task = {
+  question: 'Ты придурок?',
+  answers: [ 'Да', 'Нет', 'А ты?', 'Пошла в дупу' ],
+  correctAnswer: 0
+}
+
+const quest = document.querySelector('h2')
+quest.innerHTML = task.question
+
+const answers = document.querySelectorAll('.answer')
+console.log(task.answers[1].a1)
+answers.forEach((e, i) => {
+  e.innerHTML = task.answers[i]
+})
