55875e28aa4a701b180d6f6cd451866e1b2d7e41
Lesha Zhoglik <lehazhoglik@gmail.com>
2023-03-16 09:18:15 +0300

    терь по божески2

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
-const tasks = 
+const questions = 
 [{
-  question: `<h2>Кто на картинке?</h2><img src="leha.jpg" alt="leha">`,
+  ask: `<h2>1 Кто на картинке?</h2>`,
   answers: [ 'Кошка', 'Лёшка', 'Антошка', '1С программист' ],
   correctAnswer: 1
 },
 {
-  question: `<h2>Что будет, если дать Лёшке кружку пива?</h2>`,
+  ask: `<h2>2 Пойдём в галактику?</h2>`,
+  answers: [ 'Канеш Брооо', 'Да', 'Побежим', 'Ноуп' ],
+  correctAnswer: 0
+},
+{
+  ask: `<h2>3 В VSCode когда заменяешь (Ctrl + h) можно тоже использовать регулярные выражения, там кнопка есть) Знал?</h2>`,
+  answers: [ 'Да', 'Есцэствена', 'Отвернись', 'Мяу' ],
+  correctAnswer: 3
+},
+{
+  ask: `<h2>4 Как делы?</h2>`,
+  answers: [ 'Кул', 'Вери кул', 'Анти кул', 'Ноуп' ],
+  correctAnswer: 1
+},
+{
+  ask: `<h2>5 Что будет, если дать Лёшке кружку пива?</h2>`,
   answers: [ 'Он издаст звук ПииииуууУ', 'Он упадёт на колени', 'Он убежит в слезах в галактику?', 'Всё сразу' ],
   correctAnswer: 3
 }]
 
-const quest = document.querySelector('.ask')
-quest.innerHTML = tasks[0].question
-
+const $ask = document.querySelector('.ask')
 const $answers = document.querySelectorAll('.answer')
-$answers.forEach((e, i) => {
-  e.innerHTML = tasks[0].answers[i]
-  e.addEventListener('click', () => {
-    if (i == tasks[0].correctAnswer) {
-      quest.innerHTML = tasks[1].question
-      for (let i = 0; i < $answers.length; i++) {
-        $answers[i].innerHTML = tasks[1].answers[i]
-      }
-    }
+
+$ask.innerHTML = questions[0].ask
+Array.from($answers).map((e, i) => {
+  e.innerHTML = questions[0].answers[i]
+})
+
+let numQuest = 0
+$answers.forEach((elBtn, iBtn) => {
+  elBtn.addEventListener('click', () => {
+    if (iBtn === questions[numQuest].correctAnswer && numQuest < questions.length - 1) {
+      numQuest++
+      $ask.innerHTML = questions[numQuest].ask
+      for (let i = 0; i < $answers.length; i++) { $answers[i].innerHTML = questions[numQuest].answers[i] }
+    } 
   })
 })
+
+
+//Сделать
+//кнопки со случайным расположением
+//анимацию на правильный/не правильный ответ
+//подведение итога
+//какую ни будь полосу прогресса
+
+//классом/рефакторинг
